3d4759de46e335f9f231e611c66fef835293b2ca
Roksana Akter <142082619+Roksana18cse04@users.noreply.github.com>
2024-10-16 16:02:03 +0600

    m

--- Main.js
+++ Main.js
@@ -3,5 +3,6 @@ const s1 =require('./student');
 
 
 console.log("Hello World");
-console.log(s1.getName());
-console.log(s1.getAge());
+console.log("Name : " + s1.getName());
+console.log("Age : " + s1.getAge());
+console.log("C_GPA : " + s1.getC_GPA());
--- student.js
+++ student.js
@@ -6,5 +6,10 @@ const getAge = () =>{
     return "24";
 }
 
+const getC_GPA = () =>{
+    return "3.46 out of 4.00";
+}
+
 exports.getName = getName;
 exports.getAge = getAge;
+exports.getC_GPA = getC_GPA;
